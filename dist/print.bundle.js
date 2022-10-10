50493d4a0994bf09748a229a550c307562d336a7
teke <mutau85@gmail.com>
2022-10-10 11:43:49 +0200

    Check for linter errors

--- dist/print.bundle.js
+++ dist/print.bundle.js
@@ -1,19 +1,12 @@
-"use strict";
 (self["webpackChunkwebpack_boiler_plate"] = self["webpackChunkwebpack_boiler_plate"] || []).push([["print"],{
 
 /***/ "./src/print.js":
 /*!**********************!*\
   !*** ./src/print.js ***!
   \**********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (() => {
 
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (/* binding */ printMe)
-/* harmony export */ });
-function printMe() {
-  console.log('I get called from print.js!')
-}
+
 
 
 /***/ })
@@ -24,4 +17,4 @@ function printMe() {
 /******/ var __webpack_exports__ = (__webpack_exec__("./src/print.js"));
 /******/ }
 ]);
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHJpbnQuYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7QUFBZTtBQUNmO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWJwYWNrLWJvaWxlci1wbGF0ZS8uL3NyYy9wcmludC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCBmdW5jdGlvbiBwcmludE1lKCkge1xyXG4gIGNvbnNvbGUubG9nKCdJIGdldCBjYWxsZWQgZnJvbSBwcmludC5qcyEnKVxyXG59XHJcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHJpbnQuYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWJwYWNrLWJvaWxlci1wbGF0ZS8uL3NyYy9wcmludC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJcclxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
---
+++ src/modules/inputScore.js
@@ -0,0 +1,3 @@
+const btn = document.querySelector('.btn-click')
+const form = document.querySelector('form')
+
