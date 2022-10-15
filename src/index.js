943e9f5ecd2e1b797d57b92dac8f179a4e9ce30d
teke <mutau85@gmail.com>
2022-10-15 18:07:06 +0200

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
-import './index.css'
-import postData from './modules/post.js'
-import getData from './modules/get.js'
+import './index.css';
+import postData from './modules/post.js';
+import getData from './modules/get.js';
 
-const url =
-  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/7TGRfrete546nDR4yurtdeOa1/scores'
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/7thejendiskaoidjd87g87/scores';
 
-const name = document.querySelector('#name')
-const score = document.querySelector('#score')
-const refresh = document.querySelector('.btn')
-const addBtn = document.querySelector('.btn-click')
+const name = document.querySelector('#name');
+const score = document.querySelector('#score');
+const refresh = document.querySelector('.btn');
+const addBtn = document.querySelector('.btn-click');
 
 addBtn.addEventListener('click', (e) => {
-  e.preventDefault()
+  e.preventDefault();
 
   const leaderObj = {
     user: name.value,
     score: score.value,
-  }
+  };
 
-  postData(url, leaderObj)
+  postData(url, leaderObj);
 
-  name.value = ''
-  score.value = ''
-})
+  name.value = '';
+  score.value = '';
+});
 
 refresh.addEventListener('click', (e) => {
-  e.preventDefault()
-  getData(url)
-})
+  e.preventDefault();
+  getData(url);
+});
--- src/modules/display.js
+++ src/modules/display.js
@@ -1,19 +1,19 @@
-const newEntry = document.querySelector('.table')
+const newEntry = document.querySelector('.table');
 const display = (data) => {
   while (newEntry.hasChildNodes()) {
-    newEntry.removeChild(newEntry.lastChild)
+    newEntry.removeChild(newEntry.lastChild);
   }
-  data.result.sort((a, b) => a.score - b.score)
+  data.result.sort((a, b) => a.score - b.score);
 
   // eslint-disable-next-line no-plusplus
   for (let i = 0; i < data.result.length; i++) {
-    const newRow = document.createElement('tr')
+    const newRow = document.createElement('tr');
     newRow.innerHTML = `
         <td>${data.result[i].user}</td>
         <td>: ${data.result[i].score}</td>
-    `
-    newEntry.appendChild(newRow)
+    `;
+    newEntry.appendChild(newRow);
   }
-}
+};
 
-export default display
+export default display;
